404a30d43d4311e7547680649ce674ed0d387cc9
keystroke3 <keystroke33@gmail.com>
2019-07-31 14:12:05 +0300

    fixed fetch error because of missing subreddit value

--- fetch.py
+++ fetch.py
@@ -34,7 +34,10 @@ post_attr_file = config['settings']['post_attr_file']
 wall_data_file = config['settings']['wall_data_file']
 pictures = config['settings']['download_dir']
 d_limit = int(config['settings']['download_limit'])
-subreddit = config['settings']['subreddit']
+try:
+    subreddits = config['settings']['communities']
+except KeyError:
+    subreddits = "wallpaper+wallpapers"
 
 
 def auth():
@@ -49,7 +52,7 @@ def auth():
         commaScopes="all",
     )
     # collect data from reddit
-    wallpaper = reddit.subreddit(subreddit)
+    wallpaper = reddit.subreddit(subreddits)
 
     top_paper = wallpaper.hot(limit=d_limit)
 
