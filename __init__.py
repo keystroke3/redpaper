09d3bbba5c9c363472fe6b11f3f173d21400459e
keystroke3 <keystroke33@gmail.com>
2020-05-25 14:29:24 +0300

    Uploading to pypi

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-
-
 # Version of Redpaper
-__version__ = "Redpaper v1.0.1-beta"
+
+__version__ = "Redpaper v0.1.0"
+
--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 
-from setuptools import setup, find_packages
+from setuptools import setup
 setup(
-    name="RedPaper",
-    version="1.0",
+    name="redpaper",
+    version="0.1.0",
     install_requires=['docutils>=0.3', 'praw>=6.2.0'],
 
     # metadata to display on PyPI
     author="Teddy Okello",
     author_email="keystroke3@gmail.com",
     description="A program to download and change desktop wallpapers",
-    license="PSF",
     keywords="change wallpaper reddit in linux desktop gnome kde xfce budgie",
     url="https://github.com/keystroke3/redpaper",   # project home page, if any
+    package_dir={'': 'src'},
     project_urls={
         "Bug Tracker": "https://github.com/keystroke3/redpaper/issues",
         "Documentation": "https://github.com/keystroke3/redpaper/wiki",
         "Source Code": "https://github.com/keystroke3/redpaper",
-    }
+    },
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ]
 )
