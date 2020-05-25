31a8c499cb4c224731eeba84c75be2da14820b1f
keystroke3 <keystroke33@gmail.com>
2020-05-25 16:32:56 +0300

    changed redpaper.py location

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 [ -d "/opt/redpaper" ] && sudo rm -r /opt/redpaper
 sudo mkdir /opt/redpaper
-sudo cp redpaper.py /opt/redpaper
+sudo cp src/redpaper.py /opt/redpaper
 sudo ln -sf /opt/redpaper/redpaper.py /usr/bin/redpaper
 sudo chmod +x /opt/redpaper/redpaper.py
 sudo chmod +x /usr/bin/redpaper
